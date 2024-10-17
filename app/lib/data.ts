06266fff8c9f4198e586a9953abf34c0b4b73bc9
Magaly <fernandezgarciamagaly@gmail.com>
2024-10-17 15:10:13 +0200

    handling errors

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -140,7 +140,11 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
+  
+   
   try {
+    
+    
     const data = await sql<InvoiceForm>`
       SELECT
         invoices.id,
@@ -155,8 +159,10 @@ export async function fetchInvoiceById(id: string) {
       ...invoice,
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
-    }));
 
+      
+    }));
+    console.log(invoice)
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
